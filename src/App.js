64eaa1d99f5d92adf2f83c8569ed7fcddbe7db32
vaishakh <vaishakhvaisu@gmail.com>
2021-08-20 10:22:49 +0530

    added

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <code>WELCOME TO REACT </code>
+        </p>
+        vaishakh
+      </header>
+    </div>
+  );
+}
+
+export default App;
